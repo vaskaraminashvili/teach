8ef1f0cca35a9e5abfdde9aac75e93eae19c3a4d
vaska raminashvili <vaska.r02@gmail.com>
2021-11-25 10:55:53 +0400

    wo

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-
+require_once 'test.php';
+$sql = "SELECT * FROM products";
+$result = mysqli_query($con, $sql);
 
 
     function pd($obj)
@@ -12,18 +14,110 @@
 
     }
 ?>
-
+<!DOCTYPE html>
 <html>
-<body>
+<head>
+    <title>Test</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<form action="welcome.php" method="get">
-    <input type="hidden" name='ttt' value="hide">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="email" name="email"><br>
-Password: <input type="password" name="password"><br>
-<input type="date" name="password"><br>
-<input type="submit" value="daachire">
-</form>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <?php
+                if (mysqli_num_rows($result) > 0) {
+                  // output data of each row
+                    while($row = mysqli_fetch_assoc($result)) {
+            ?>
+
+
+            <?php
+                    }
+                } else {
+                  echo "0 results";
+                }
+            ?>
+            <div class="col-xs-12 col-md-6 col-xl-4 mb-5">
+                <div class="card">
+                  <img src="https://www.apple.com/v/product-red/r/images/meta/og__dbjwy50zuc02.png?202106010703" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-xl-4 mb-5">
+                <div class="card">
+                  <img src="https://www.apple.com/v/product-red/r/images/meta/og__dbjwy50zuc02.png?202106010703" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-xl-4 mb-5">
+                <div class="card">
+                  <img src="https://www.apple.com/v/product-red/r/images/meta/og__dbjwy50zuc02.png?202106010703" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-xl-4 mb-5">
+                <div class="card">
+                  <img src="https://www.apple.com/v/product-red/r/images/meta/og__dbjwy50zuc02.png?202106010703" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 
 </body>
 </html>
