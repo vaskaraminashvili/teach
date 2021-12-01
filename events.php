321ffb5fc8830b87c02616d8170cf6787124b4ff
vaska raminashvili <vaska.r02@gmail.com>
2021-12-01 10:55:03 +0400

    wip

--- events.php
+++ events.php
@@ -1,7 +1,75 @@
 <?php
-require_once 'test.php';
 
-$sql = "SELECT * FROM events";
-$result = mysqli_query($con, $sql);
+$string = '1000';
+$number = 100;
+// $arr = array(); //masivi
+// $arr = array(
+//     $number,
+//     $string,
+//     234423,
+//     array(
+//         'asdasd',
+//         '1 asdasd',
+//         '2 asdasd',
+//         '3 asdasd',
+//         '4 asdasd',
+//     )
+// );
+// $arr = [
+//     $number,
+//     $string,
+//     234423,
+//     [
+//         'asdasd',
+//         '1 asdasd',
+//         '2 asdasd',
+//         '3 asdasd',
+//         '4 asdasd',
+//         [
+//             'measme done',
+//             [
+//                 'amomige me',
+//                 ' asd 23',
+//             ]
+//         ]
+//     ]
+// ];
 
-?>
+// foreach ($arr as $key => $value1) {
+
+//     if (is_array($value1)) {
+//         foreach ($value1 as $value2) {
+
+//             if (is_array($value2)) {
+//                 foreach ($value2 as $value3) {
+
+//                     if (is_array($value3)) {
+//                         foreach ($value3 as $value4) {
+
+//                             echo ($value4);
+//                             echo "<br>";
+//                             // die();
+
+//                         }
+//                     }
+
+//                 }
+//             }
+
+//         }
+//     }
+
+// }
+
+
+// var_dump($arr[3][5][1][0]);
+
+
+// 1.  sheqmeni carieli masivvi   $arr
+// 2.  masivshi chatsere 5 elementi zogis iyos STRING tipis zogi INT tipis
+
+// 3. arsebul masivshi boloshi daamate axali elementi romelic iqneba MASIVI/ARRAY
+
+// 4. 3 davalebashi sheqmnil masivshi chatsere kide elementebi
+
+// 5. var dampit dabecvhde mtliani array
