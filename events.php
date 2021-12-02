a8d3b753cbfbf02b012134e5b609fe72d5f3362f
Vaska Raminashvili <vaska.r02@gmail.com>
2021-12-02 10:28:50 +0400

    lesson2

--- events.php
+++ events.php
@@ -1,7 +1,7 @@
 <?php
 
-$string = '1000';
-$number = 100;
+// $string = '1000';
+// $number = 100;
 // $arr = array(); //masivi
 // $arr = array(
 //     $number,
@@ -73,3 +73,134 @@ $number = 100;
 // 4. 3 davalebashi sheqmnil masivshi chatsere kide elementebi
 
 // 5. var dampit dabecvhde mtliani array
+
+// ============================================================= next lessson =============================================================
+
+
+// $arr = [1213 , 'asdasd',  [1,2,3, ['aqvar']]];
+// var_dump($arr);
+// array_push($arr[2][3],"blue","yellow");
+// var_dump($arr);
+
+
+// $arr = [
+//     'peter' => [
+//         'salary' => '500',
+//         'car' => 'bmw',
+//         'houese' => 'kargi',
+//         'skills' => [
+//             'php',
+//             'html',
+//             'css',
+//         ],
+//     ],
+//     'john' => [
+//         'salary' => '800',
+//         'car' => 'bmw',
+//         'houese' => 'kargi',
+//         'skills' => [
+//             'js',
+//             'html',
+//             'css',
+//         ],
+//     ],
+// ];
+
+// foreach ($arr as $key => $value) {
+//     if ($key == 'peter') {
+//         echo $key . ' asdasds';
+//         echo "<br>";
+//         echo "<br>";
+//         echo "<br>";
+//     }
+//         echo $key;
+//         echo "<br>";
+// }
+
+$arr = [
+'Asia' => [
+    'China',
+    'India',
+    'Indonesia',
+    'Pakistan',
+],
+'Africa' => [
+    'China 1',
+    'India 2',
+    'Indonesia 3',
+    'Pakistan4',
+]
+// 'Europe' =>
+// 'North America' =>
+// 'South America' =>
+// 'Australia' =>
+// 'Antarctica' =>
+];
+// var_dump($arr);
+
+// foreach ($arr as $continet => $value) {
+//     if ($continet == 'Africa') {
+//         # code...
+//         echo $continet;
+//         echo "<br>";
+//         echo "<br>";
+//         echo "<br>";
+//         foreach ($value as $asdasd) {
+//            echo $asdasd;
+//             echo "<br>";
+//         }
+//     }
+// }
+// var_dump($arr['Africa'][2]);
+#   Country Population
+
+
+// davaleba
+// gaakete 3 doniani Associative  array
+// $arr = [
+//     'mer' => [
+//         'model 1' => [
+//             'hp'
+//         ],
+//         'model 2' => [
+//             'hp'
+//         ]
+//     ]
+// ]
+
+// $cars = [
+//     'bmw' => [
+//         'x4' => [
+//             'hp',
+//             'doors',
+//             'doors',
+//             'doors',
+//             'doors',
+//         ],
+//     ],
+// ];
+// foreach ($cars as $comany => $models) {
+//     echo $comany;
+//     echo "<br>";
+//     foreach ($models as $model => $specs) {
+//         echo $model;
+//         echo "<br>";
+//         foreach ($specs as $erti) {
+//             echo $erti;
+//             echo "<br>";
+//         }
+
+//     }
+
+// }
+// $ragaca = $cars['bmw']['x4'];
+// foreach ($ragaca as $erti) {
+//     echo $erti;
+//     echo "<br>";
+// }
+// var_dump($cars['bmw']);
+
+//foreach each dabechde yvelaperi
+// merc
+// bmw-
+// fiat
